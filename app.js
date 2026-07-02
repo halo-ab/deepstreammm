244f75b632b75f99838653bdca83e6dec9de640f
halo-ab <d33717154@gmail.com>
2026-07-02 12:36:31 +0530

    Update playlist URL for FanCode Auto Update

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ let hls = null;
 let networkRetries = 0;
 
 const PLAYLIST_URL =
-  "https://raw.githubusercontent.com/drmlive/fancode-live-events/refs/heads/main/fancode.m3u";
+  "https://raw.githubusercontent.com/sm-monirulislam/FanCode-Auto-Update-Playlist/refs/heads/main/fancode_in.m3u";
 const PLAYLIST_REFRESH_MS = 5 * 60 * 1000;
 
 /** @param {string} url */
